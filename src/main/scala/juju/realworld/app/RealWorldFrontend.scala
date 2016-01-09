fe175caedb019727135e2ad11cb548fa01635cd3
rucka <rucka@tiscalinet.it>
2016-01-09 15:10:23 +0100

    wip on frontend rest services

--- src/main/scala/juju/realworld/app/RealWorldFrontend.scala
+++ src/main/scala/juju/realworld/app/RealWorldFrontend.scala
@@ -3,28 +3,39 @@ package juju.realworld.app
 import akka.actor.{Actor, ActorLogging}
 import juju.messages.Command
 import realworld.domain.Book.CreateBook
+import realworld.domain.Manager.CreateManager
+import realworld.domain.Order.PlaceOrder
 import spray.http.{HttpEntity, MediaTypes}
-import spray.httpx.unmarshalling.Unmarshaller
+import spray.httpx.unmarshalling.{FromRequestUnmarshaller, Unmarshaller}
 import spray.routing.{HttpService, Route}
 
 import scala.reflect.ClassTag
 import scala.reflect.runtime.{universe => ru}
 
-class RealWorldFrontend extends Actor with ActorLogging with BackendService {
+class RealWorldFrontend extends FrontendService {
+  implicit def unmarshallerCommand[T <: Command : ClassTag] = CommandService.unmarshallerCommand
 
+  override val backendApiRoute: Route =
+    pathPrefix("api") {
+      post {
+        path("book") {handleCommand[CreateBook]} ~ path("manager") {handleCommand[CreateManager]} ~ path("order") {handleCommand[PlaceOrder]}
+      }
+    }
+}
+
+trait FrontendService extends Actor with ActorLogging with CommandService {
   def actorRefFactory = context
 
   def receive = runRoute(backendApiRoute)
 }
 
-trait BackendService extends HttpService {
+object CommandService {
   implicit def unmarshallerCommand[T <: Command : ClassTag] = Unmarshaller[T](MediaTypes.`application/x-www-form-urlencoded`) {
     case e: HttpEntity.NonEmpty => {
       val u = spray.httpx.unmarshalling.FormDataUnmarshallers.UrlEncodedFormDataUnmarshaller(e)
       u match {
         case Right(data) => {
           val commandType = implicitly[ClassTag[T]].runtimeClass
-          //val parameters = data.fields.map(_._2)
           val map = data.fields.toMap
 
           val properties = getCaseClassParameters[T]
@@ -44,17 +55,7 @@ trait BackendService extends HttpService {
     }
   }
 
-  val backendApiRoute: Route = {
-    (pathPrefix("api") & post & path("book")) {
-      entity(as[CreateBook]) { command =>
-        complete {
-             s"received command to create book ${command.isbn} / ${command.title} / ${command.author} \n"
-          }
-      }
-    }
-  }
-
-  def companionMembers(clazzTag: scala.reflect.ClassTag[_]): ru.MemberScope = {
+  private def companionMembers(clazzTag: scala.reflect.ClassTag[_]): ru.MemberScope = {
     val runtimeClass = clazzTag.runtimeClass
     val rootMirror = ru.runtimeMirror(runtimeClass.getClassLoader)
     val classSymbol = rootMirror.classSymbol(runtimeClass)
@@ -63,9 +64,21 @@ trait BackendService extends HttpService {
   }
 
 
-  def getCaseClassParameters[T : ClassTag] =
+  private def getCaseClassParameters[T : ClassTag] =
     companionMembers(scala.reflect.classTag[T])
       .filter { m => m.isMethod && m.name.toString == "apply"}
       .head.asMethod.paramLists.head.map(p => (p.name, p.info)).toSeq
 
+}
+
+trait CommandService extends HttpService {
+  val backendApiRoute: Route
+
+  protected def handleCommand[C <: Command : ClassTag : FromRequestUnmarshaller] = {
+    entity(as[C]) { command =>
+      complete {
+        s"received command $command \n"
+      }
+    }
+  }
 }
